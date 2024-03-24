b76f0f74967981e88df80eec4638bf66ae41608f
blurz <ismailerenozcinar@gmail.com>
2024-03-24 22:13:47 +0300

    blood added

--- emu gamejam/Assets/Scripts/bullet.cs
+++ emu gamejam/Assets/Scripts/bullet.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     public GameObject hitEffect;
+    public GameObject Blood;
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
         if(collision.gameObject.TryGetComponent<enemyDamage>(out enemyDamage enemyComponent))
         {
             enemyComponent.TakeDamage(1);
+            Instantiate(Blood, transform.position, Quaternion.identity);
         }
 
         if (collision.gameObject.TryGetComponent<playerDamage>(out playerDamage playerComponent))
         {
             playerComponent.TakeDamage(1);
+            Instantiate(Blood, transform.position, Quaternion.identity);
         }
 
         Destroy(gameObject);
